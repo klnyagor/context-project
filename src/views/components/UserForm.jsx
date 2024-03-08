a7cf5f3307794bfa4ce2ec9333ecdb2b7664c349
klnyagor <kalenieves.yagor@academico.ifpb.edu.br>
2024-03-08 15:35:16 -0300

    Basic Formik

--- src/views/components/UserForm.jsx
+++ src/views/components/UserForm.jsx
@@ -1,5 +1,6 @@
+import { Formik } from 'formik';
 import { useUser } from '../../contexts/UserContext';
-import { Button, Form, Modal } from 'react-bootstrap';
+import { Alert, Button, Form, Modal } from 'react-bootstrap';
 
 const UserForm = () => {
   let { showModal, handleModal } = useUser();
@@ -8,38 +9,87 @@ const UserForm = () => {
     <>
       <Modal show={showModal} onHide={handleModal}>
         <Modal.Header closeButton>
-          <Modal.Title>Novo Usuario</Modal.Title>
+          <Modal.Title as="h5">Novo Usuario</Modal.Title>
         </Modal.Header>
-        <Form>
-          <Modal.Body>
-            <Form.Group>
-              <Form.Label>Nome</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Digite o Nome de usuário"
-                name="nome"
-                onChange={() => {}}
-              />
-            </Form.Group>
-            <Form.Group>
-              <Form.Label>Email</Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="Digite o E-mail"
-                name="email"
-                onChange={() => {}}
-              />
-            </Form.Group>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant="danger" onClick={handleModal}>
-              Fechar
-            </Button>
-            <Button variant="success" type="submit">
-              Confirmar
-            </Button>
-          </Modal.Footer>
-        </Form>
+        <Formik
+          initialValues={{ nome: '', email: '' }}
+          validate={(values) => {
+            const errors = {};
+            if (!values.nome) {
+              errors.nome = 'Required';
+            }
+            if (!values.email) {
+              errors.email = 'Required';
+            } else if (
+              !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+            ) {
+              errors.email = 'Invalid email address';
+            }
+            return errors;
+          }}
+          onSubmit={(values, { setSubmitting }) => {
+            setTimeout(() => {
+              alert(JSON.stringify(values, null, 2));
+              setSubmitting(false);
+            }, 400);
+          }}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            isSubmitting,
+            /* and other goodies */
+          }) => (
+            <Form onSubmit={handleSubmit}>
+              <Modal.Body>
+                <Form.Group>
+                  <Form.Label>Nome</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Digite o Nome de usuário"
+                    name="nome"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.nome}
+                  />
+                  {!errors.nome || (
+                    <Alert variant="danger">
+                      {errors.nome && touched.nome && errors.nome}
+                    </Alert>
+                  )}
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control
+                    type="email"
+                    placeholder="Digite o E-mail"
+                    name="email"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.email}
+                  />
+                  {!errors.email || (
+                    <Alert variant="danger">
+                      {errors.email && touched.email && errors.email}
+                    </Alert>
+                  )}
+                </Form.Group>
+              </Modal.Body>
+              <Modal.Footer>
+                <Button variant="danger" onClick={handleModal}>
+                  Fechar
+                </Button>
+                <Button variant="success" type="submit" disabled={isSubmitting}>
+                  Confirmar
+                </Button>
+              </Modal.Footer>
+            </Form>
+          )}
+        </Formik>
       </Modal>
     </>
   );
